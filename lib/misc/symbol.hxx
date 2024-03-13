b76c7ba408f526cc130d93ef5ba29397b7f1ef15
Sanghyeon PARK <sanghyeon.park@gmail.com>
2024-03-13 16:28:09 +0100

    fix: unique and symbol

--- lib/misc/symbol.hxx
+++ lib/misc/symbol.hxx
@@ -11,22 +11,31 @@ namespace misc
 {
   inline symbol& symbol::operator=(const symbol& rhs)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    if (*this != rhs)
+    {
+        this->obj_ = &rhs.get();
+    }
+    return *this;
   }
 
   inline bool symbol::operator==(const symbol& rhs) const
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    return this->get() == rhs.get();
   }
 
   inline bool symbol::operator!=(const symbol& rhs) const
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    return this->get() != rhs.get();
   }
 
   inline std::ostream& operator<<(std::ostream& ostr, const symbol& the)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    ostr << the.get();
+    return ostr;
   }
 
 } // namespace misc
--- lib/misc/unique.hxx
+++ lib/misc/unique.hxx
@@ -11,7 +11,8 @@
 namespace misc
 {
   template <typename T, class C> unique<T, C>::unique(const data_type& s)
-    :obj_(&(*object_set_instance().insert(s).first))// FIXME: Some code was deleted here (Initializations).
+  // FIXED: Some code was deleted here (Initializations).
+    :obj_(&(*object_set_instance().insert(s).first))
   /** \brief Following the Flyweight design pattern, set the attribute to a
        unique reference of value s. You might want to check out std::set methods
        on cppreference.com. */
@@ -20,29 +21,34 @@ namespace misc
   template <typename T, class C>
   typename unique<T, C>::object_set_type& unique<T, C>::object_set_instance()
   {
+    // FIXED: Some code was deleted here (Classical Singleton pattern, a la Scott Meyers').
     static object_set_type* set = nullptr;
     if (set == nullptr)
       set = new object_set_type();
-    return *set;// FIXME: Some code was deleted here (Classical Singleton pattern, a la Scott Meyers').
+      
+    return *set;
     /** \brief Create a persistent instance of a set which would hold each value. */
   }
 
   template <typename T, class C>
   typename unique<T, C>::object_size_type unique<T, C>::object_map_size()
   {
-    return object_set_instance().size();// FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    return object_set_instance().size();
   }
 
   template <typename T, class C>
   inline const typename unique<T, C>::data_type& unique<T, C>::get() const
   {
-    return *obj_;// FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    return *obj_;
   }
 
   template <typename T, class C>
   inline unique<T, C>::operator const data_type&() const
   {
-    return get();// FIXME: Some code was deleted here (Check Typecasts overload).
+  // FIXED: Some code was deleted here (Check Typecasts overload).
+    return get();
   }
 
   template <typename T, class C>
